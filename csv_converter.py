da9c491317261d4628be4be0a5a4c61a65c5db55
Dave Stonecipher <stonecipherdr@gmail.com>
2022-07-28 14:15:08 -0600

    Initial commit

---
+++ csv_converter.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+"""
+    A helper script for when data is mostly stored in exported excel files.
+    Move to the directory/drive where data has been saved in XLSX or CSV files. Convert all XLSX into CSV and delete the old files afterwards.
+"""
+
+if "__name__" == "__main__":
+    main()
