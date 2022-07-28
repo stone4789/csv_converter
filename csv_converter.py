15c7471bdaaf7ab0ac77f242bc71a2a9f787b729
Dave Stonecipher <stonecipherdr@gmail.com>
2022-07-28 14:26:02 -0600

    Added imports, glob for file list

--- csv_converter.py
+++ csv_converter.py
@@ -4,5 +4,22 @@
     Move to the directory/drive where data has been saved in XLSX or CSV files. Convert all XLSX into CSV and delete the old files afterwards.
 """
 
+
+def main():
+    import openpyxl
+    import warnings
+
+    warnings.filterwarnings("ignore")
+    import glob
+    import pandas as pd
+    import os
+
+    folder_path = # put string directory path for where the data's stored here
+    os.chdir(folder_path)
+
+    path = os.cwd()
+    excel_files = glob.glob(os.path.join(path, "*.xlsx")) 
+
+
 if "__name__" == "__main__":
     main()
